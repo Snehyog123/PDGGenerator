6646809201eaeb66407e972d5901d513e66f8f3d
suyog <suyogshirsath11@gmail.com>
2023-03-30 11:01:52 +0530

    Revert "Revert "User Controller Test Added""
    This reverts commit 03ce64993bbdb4b751644ff7bd6f50c87fa91858.

---
+++ src/test/java/com/infopper/pdfgenerator/controller/UserControllerTest.java
@@ -0,0 +1,20 @@
+package com.infopper.pdfgenerator.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void getAllUser() {
+    }
+
+    @Test
+    void exportToPdf() {
+    }
+}
